e677f992c35b409c274efd6eb07d5e512072ccc6
mlaga97 <mlaga97@gmail.com>
2015-01-12 15:49:02 -0600

    Add some setup stuff.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,10 +1,26 @@
 #include "WPILib.h"
 
+#define	frontLeft	1
+#define	rearLeft	2
+#define	frontRight	0
+#define	rearRight	3
+
+#define	joystickChannel	0
+
 class Robot: public IterativeRobot
 {
+private:	
+	RobotDrive robotDrive;	// robot drive system
+	Joystick stick;			// only joystick
+	
 public:
 	// Constructor Method
-	Robot();
+	Robot():
+		robotDrive(frontLeft, rearLeft, frontRight, rearRight),	// Must be initialized in the proper order
+		stick(joystickChannel)
+	{
+		
+	}
 
 	// Various Iterators
 	void RobotInit();
