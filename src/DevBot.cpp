77eb347d9e047662d1aa089d338f5d5e5e882e64
mlaga97 <mlaga97@gmail.com>
2015-01-27 01:05:25 -0600

    Refactor DevBot class definition to header file.

--- src/DevBot.cpp
+++ src/DevBot.cpp
@@ -2,24 +2,6 @@
 #include "WPILib.h"
 #include "DevBot.h"
 
-class DevBot: public IterativeRobot
-{
-private:	
-	RobotDrive robotDrive;	// robot drive system
-	Joystick stick;		// only joystick
-	
-public:
-	// Constructor Method
-	DevBot();
-
-	// Various Iterators
-	//void RobotInit();
-	//void AutonomousInit();
-	//void AutonomousPeriodic();
-	void TeleopInit();
-	void TeleopPeriodic();
-};
-
 DevBot::DevBot():
 	robotDrive(frontLeft, rearLeft, frontRight, rearRight),	// Must be initialized in the proper order
 	stick(joystickChannel)
--- src/DevBot.h
+++ src/DevBot.h
@@ -10,4 +10,23 @@
 // Joystick IDs
 #define	joystickChannel	0
 
+// Main Robot Class
+class DevBot: public IterativeRobot
+{
+private:	
+	RobotDrive robotDrive;	// robot drive system
+	Joystick stick;		// only joystick
+	
+public:
+	// Constructor Method
+	DevBot();
+
+	// Various Iterators
+	//void RobotInit();
+	//void AutonomousInit();
+	//void AutonomousPeriodic();
+	void TeleopInit();
+	void TeleopPeriodic();
+};
+
 #endif /* SRC_DEVBOT_H_ */
