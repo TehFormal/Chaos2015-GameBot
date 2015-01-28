f79ef9e5a8a934af2c4ff8ba5b88638d368e0478
mlaga97 <mlaga97@gmail.com>
2015-01-28 11:24:21 -0600

    Add a more proper control loop for the arm.

--- src/Teleop.cpp
+++ src/Teleop.cpp
@@ -12,14 +12,51 @@ void DevBot::TeleopPeriodic() {
 		-0.5*stick.GetRawAxis(1),		// Forward movement
 		-0.5*stick.GetRawAxis(4)		// Rotational movement
 	);
-
-	// Solenoid A
-	solenoid0.Set( stick.GetRawButton(1) );
-	solenoid7.Set( stick.GetRawButton(2) );
-
-	// Solenoid B
-	solenoid1.Set( stick.GetRawButton(3) );
-	solenoid6.Set( stick.GetRawButton(4) );
+	
+	// Control Loop for Arm
+	//
+	// This is in the form of an if-then-elseif loop to prevent the buttons from interfering with
+	// one another and to allow for proper resetting.
+	//
+	// NOTE: For double-acting solenoids, you should always turn off the opposite solenoid before
+	//	turning on the one you are trying to activate, so your false's should precede true's in
+	//	loops similar to this one.
+	if( stick.GetRawButton(0) )		// If the A button is being pressed, move down
+	{
+		// Set the state of solenoid A
+		solenoid0.Set(false);
+		solenoid7.Set(false);
+		
+		// Set the state of solenoid B
+		solenoid1.Set(false);
+		solenoid6.Set(true);
+	
+		stopped = false; // Remember that we aren't stopped
+	} else if ( stick.GetRawButton(1) )	// Otherwise, if the B button in being pressed, move up
+	{
+		// Set the state of solenoid A 
+		solenoid7.Set(false);
+		solenoid0.Set(true);
+		
+		// Set the state of solenoid B
+		solenoid6.Set(false);
+		solenoid1.Set(true);
+		
+		// Reset stopped flag
+		stopped = false;
+	} else if ( stopped == false )		// If neither is pressed, and we aren't already stopped, stop.
+	{
+		// Set the state of solenoid A 
+		solenoid0.Set(false);
+		solenoid7.Set(true);
+		
+		// Set the state of solenoid B
+		solenoid6.Set(false);
+		solenoid1.Set(true);
+		
+		// Set stopped flag
+		stopped = true;
+	}
 
 	Wait(0.005); // Give the CPU some time to breathe so it doesn't explode.
 }
