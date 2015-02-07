e724f3cee85881d9dd0356f7da94c2b14612b9e4
mlaga97 <mlaga97@gmail.com>
2015-02-07 15:32:41 -0600

    Add arm library code.

---
+++ src/Arm.h
@@ -0,0 +1,27 @@
+#ifndef SRC_ARM_H_
+#define SRC_ARM_H_
+
+#include "WPILib.h"
+
+class Arm
+{
+private:
+	// Solenoid A
+	Solenoid aLow;
+	Solenoid aHigh;
+
+	// Solenoid B
+	Solenoid bLow;
+	Solenoid bHigh;
+
+	// Flag Variables
+	int state;
+
+public:
+	Arm( int _aLow, int _aHigh, int _bLow, int _bHigh );
+
+	// Member Methods
+	void Set( int _state );
+};
+
+#endif /* SRC_ARM_H_ */
