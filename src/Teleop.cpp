79442d6c4693c70af85132e9e5201ab7b166c2f0
mlaga97 <mlaga97@gmail.com>
2015-02-11 19:10:54 -0600

    Add teleop code for arms.

--- src/Teleop.cpp
+++ src/Teleop.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include "WPILib.h"
 #include "DevBot.h"
+#include "Arm.h"
 
 void DevBot::TeleopInit() {
 	robotDrive.SetSafetyEnabled(false);	// Necessary for proper motor functioning during Teleop
@@ -19,6 +20,24 @@ void DevBot::TeleopPeriodic() {
 		grabber.Set( DoubleSolenoid::kForward );
 	} else if( copilot.GetRawButton(6) ) {
 		grabber.Set( DoubleSolenoid::kReverse );
+	}
+
+	// Right Arm
+	if( copilot.GetRawButton(1) ) {
+		rightArm.Set( -1 );
+	} else if( copilot.GetRawButton(2) ) {
+		rightArm.Set( 1 );
+	} else {
+		rightArm.Set( 0 );
+	}
+
+	// Left Arm
+	if( copilot.GetRawButton(3) ) {
+		leftArm.Set( -1 );
+	} else if( copilot.GetRawButton(4) ) {
+		leftArm.Set( 1 );
+	} else {
+		leftArm.Set( 0 );
 	}
 
 	Wait(0.005); // Give the CPU some time to breathe so it doesn't explode.
