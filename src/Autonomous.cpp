50789b4324d0d9b78bdaee66dfd650634df73a03
mlaga97 <mlaga97@gmail.com>
2015-02-13 14:40:20 -0600

    Make DevBot::Forward more like DevBot::Backward.

--- src/Autonomous.cpp
+++ src/Autonomous.cpp
@@ -35,18 +35,18 @@ void DevBot::Forward( float Speed, float Time ) {
 	// Reset the gyro to 0 degrees
 	gyro.Reset();
 
-	// Timer Stuff
+	// Initialize Timer
 	Timer timer;
 	timer.Reset();
 	timer.Start();
 
+	// Move straight, changing angle to adjust for drift
 	while ( timer.Get() <= Time ) {
-		float angle = gyro.GetAngle();
-		//cout << "\t" << timer.Get() << "\n";
-		robotDrive.Drive( Speed, -angle * 0.03 );
+		robotDrive.MecanumDrive_Cartesian( 0, Speed, -gyro.GetAngle() * 0.1 );
 		Wait( 0.005 );
 	}
-	robotDrive.Drive( 0, 0 );
-
+	
+	// Leave everything as we found it
+	robotDrive.MecanumDrive_Cartesian( 0, 0, 0 );
 	timer.Stop();
 }
