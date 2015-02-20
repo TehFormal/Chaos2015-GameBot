764c887b9bf64ff1bda0bb134d63f0b5eac7081a
mlaga97 <mlaga97@gmail.com>
2015-02-19 20:02:07 -0600

    Add second motor to LimitedForklift.

--- src/LimitedForklift.cpp
+++ src/LimitedForklift.cpp
@@ -1,8 +1,9 @@
 #include "WPILib.h"
 #include "LimitedForklift.h"
 
-LimitedForklift::LimitedForklift( int talon_id, int topLS_port, int bottomLS_port ):
+LimitedForklift::LimitedForklift( int talon_id, int talon_id2, int topLS_port, int bottomLS_port ):
 	forkliftMotor( talon_id ),
+	forkliftMotor2( talon_id2 ),
 	topLS( topLS_port ),
 	bottomLS( bottomLS_port )
 {
@@ -16,8 +17,10 @@ void LimitedForklift::Set( float speed ) {
 		// If the top limit switch is tripped, only go down.
 		if( speed < 0 ) {
 			forkliftMotor.Set( speed );
+			forkliftMotor2.Set( speed );
 		} else {
 			forkliftMotor.Set( 0 );
+			forkliftMotor2.Set( 0 );
 		}
 
 	} else if( bottomLS.Get() ) {
@@ -25,14 +28,17 @@ void LimitedForklift::Set( float speed ) {
 		// If the bottom limit switch is tripped, only go up.
 		if( speed > 0 ) {
 			forkliftMotor.Set( speed );
+			forkliftMotor2.Set( speed );
 		} else {
 			forkliftMotor.Set( 0 );
+			forkliftMotor2.Set( 0 );
 		}
 
 	} else {
 
 		// Otherwise, go whatever speed is given.
 		forkliftMotor.Set( speed );
+		forkliftMotor.Set2( speed );
 
 	}
 
