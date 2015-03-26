ed8b98492034c2b0505910d32bb0e23c306e2cf4
mlaga97 <mlaga97@gmail.com>
2015-03-26 09:30:31 -0500

    Refactor and comment autonomous code.

--- src/Autonomous.cpp
+++ src/Autonomous.cpp
@@ -51,125 +51,58 @@
 }*/
 
 void DevBot::AutonomousInit() {
+	// Setup
+	robotDrive.SetSafetyEnabled( false );
+	gyro.Reset();
+	Timer timer;
+
+	// Start off autonomous by setting down arms
 	rightArm.Set( -1 );
 	leftArm.Set( -1 );
 
-	///////////////////////////////////////////////////////////////////////////////
-
-	robotDrive.SetSafetyEnabled( false );
-
-	// Reset the gyro to 0 degrees
-	gyro.Reset();
-
-	// Initialize Timer
-	Timer timer;
+	// Reset Timer for loop
 	timer.Reset();
 	timer.Start();
 
-	// Move straight, changing angle to adjust for drift
+	// Move straight into landfill platform using gyroscope and accelerometer.
 	while ( accelerometer.GetX() > -0.75 || timer.Get() < 2 ) {
 		robotDrive.MecanumDrive_Cartesian( 0, -0.20, gyro.GetAngle() * 0.1 );
-
-		// Set real motor values based off of the fake ones
-		frontRight.Set(PWMfr.Get());
-		frontLeft.Set(PWMfl.Get());
-		rearLeft.Set(PWMrl.Get());
-		rearRight.Set(PWMrr.Get());
-
+		UpdateMotors();
 		Wait( 0.005 );
 	}
 
-	// Leave everything as we found it
-	robotDrive.MecanumDrive_Cartesian( 0, 0, 0 );
+	// Apply a little more pressure to compensate for bouncing off the platform
+	robotDrive.MecanumDrive_Cartesian( 0, -0.1, 0 );
+	UpdateMotors();
 
-	// Set real motor values based off of the fake ones
-	frontRight.Set(PWMfr.Get());
-	frontLeft.Set(PWMfl.Get());
-	rearLeft.Set(PWMrl.Get());
-	rearRight.Set(PWMrr.Get());
-
-	timer.Stop();
-
-	/////////////////////////////////////////////////////////////////////////////////////////
-
-	// Leave everything as we found it
-	robotDrive.MecanumDrive_Cartesian( 0.1, 0, 0 );
-
-	// Set real motor values based off of the fake ones
-	frontRight.Set(PWMfr.Get());
-	frontLeft.Set(PWMfl.Get());
-	rearLeft.Set(PWMrl.Get());
-	rearRight.Set(PWMrr.Get());
-
+	// Continue applying pressure
 	Wait( 0.5 );
 
-	// Leave everything as we found it
+	// Set speed back to zero
 	robotDrive.MecanumDrive_Cartesian( 0, 0, 0 );
+	UpdateMotors();
 
-	// Set real motor values based off of the fake ones
-	frontRight.Set(PWMfr.Get());
-	frontLeft.Set(PWMfl.Get());
-	rearLeft.Set(PWMrl.Get());
-	rearRight.Set(PWMrr.Get());
-
+	// Lift Arms Up
 	rightArm.Set( 1 );
 	leftArm.Set( 1 );
 
+	// Wait for arms to engage cans
 	Wait( 2 );
 
+	// Hold Arms in place
 	rightArm.Set( 0 );
 	leftArm.Set( 0 );
 
-	/////////////////////////////////////////////////////////////////////////////////////////
+	// Bring cans forward to landfill zone
+	robotDrive.MecanumDrive_Cartesian( 0, 0.35, 0 );
+	UpdateMotors();
 
-
-
-	robotDrive.SetSafetyEnabled( false );
-
-	// Reset the gyro to 0 degrees
-	gyro.Reset();
-
-	// Initialize Timer
-	//Timer timer;
-	timer.Reset();
-	timer.Start();
-
-	/*
-	// Move straight, changing angle to adjust for drift
-	while ( accelerometer.GetX() < 0.75 || timer.Get() < 2 ) {
-		robotDrive.MecanumDrive_Cartesian( 0, 0.25, -gyro.GetAngle() * 0.1 );
-
-		// Set real motor values based off of the fake ones
-		frontRight.Set(PWMfr.Get());
-		frontLeft.Set(PWMfl.Get());
-		rearLeft.Set(PWMrl.Get());
-		rearRight.Set(PWMrr.Get());
-
-		Wait( 0.005 );
-	}
-	*/
-
-	// Leave everything as we found it
-	robotDrive.MecanumDrive_Cartesian( 0.25, 0, 0 );
-
-	// Set real motor values based off of the fake ones
-	frontRight.Set(PWMfr.Get());
-	frontLeft.Set(PWMfl.Get());
-	rearLeft.Set(PWMrl.Get());
-	rearRight.Set(PWMrr.Get());
-
+	// Wait until we get there
 	Wait(0.5);
 
-	// Leave everything as we found it
+	// Stop
 	robotDrive.MecanumDrive_Cartesian( 0, 0, 0 );
-
-	// Set real motor values based off of the fake ones
-	frontRight.Set(PWMfr.Get());
-	frontLeft.Set(PWMfl.Get());
-	rearLeft.Set(PWMrl.Get());
-	rearRight.Set(PWMrr.Get());
-
-	timer.Stop();
+	UpdateMotors();
 }
 
 void DevBot::AutonomousPeriodic() {
